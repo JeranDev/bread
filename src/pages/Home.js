74e766508d2cad7af3f23ab7a7ba68489b685544
JeranDev <jeranjb@gmail.com>
2020-12-09 18:36:08 -0700

    add game detail data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import GameDetail from '../components/GameDetail'
 //Redux
 import { useDispatch, useSelector } from 'react-redux'
 import { loadGames } from '../actions/gamesAction'
@@ -18,6 +19,7 @@ const Home = () => {
   const { popular, upcoming, newGames } = useSelector(state => state.games)
   return (
     <GameList>
+      <GameDetail />
       <h2>Upcoming Games</h2>
       <Games>
         {upcoming.map(game => (

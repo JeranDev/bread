0c657a418b1e6a525a9a60a87b06a654799cdbeb
JeranDev <jeranjb@gmail.com>
2020-12-11 13:30:25 -0700

    add motion to game and gameDetail

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,6 +8,7 @@ import { Link, useHistory } from 'react-router-dom'
 import { smallImage } from '../util'
 
 const Game = ({ name, released, image, id }) => {
+  const stringPathId = id.toString()
   //Fix Scrolling
   const history = useHistory()
   if (history.location.pathname === '/') {
@@ -23,11 +24,15 @@ const Game = ({ name, released, image, id }) => {
   }
 
   return (
-    <StyledGame onClick={loadDetailHandler}>
+    <StyledGame layoutId={stringPathId} onClick={loadDetailHandler}>
       <Link to={`/game/${id}`}>
-        <h3>{name}</h3>
+        <motion.h3 layoutId={`title ${stringPathId}`}>{name}</motion.h3>
         <p>{released}</p>
-        <img src={smallImage(image, 640)} alt={name} />
+        <motion.img
+          layoutId={`image ${stringPathId}`}
+          src={smallImage(image, 640)}
+          alt={name}
+        />
       </Link>
     </StyledGame>
   )
